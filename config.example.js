fcf1120adfdf1046cf8a03ac15cc063b8a9cc628
MutazGhazal <162821289+MutazGhazal@users.noreply.github.com>
2026-02-08 02:24:40 +0300

    Update CLIENT_CONFIG with new Supabase credentials
    save

--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 window.CLIENT_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://molvqpeiqeifgaiczpdu.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vbHZxcGVpcWVpZmdhaWN6cGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0ODQ0NTksImV4cCI6MjA4NjA2MDQ1OX0.5-NCtxmscTbP-q9XPx0mrm5iAuGSjSZj9q9dDbUIzOo",
   BUCKET: "client-media",
 };
+
